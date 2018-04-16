71870029c4085916ca17aef00def5f626da82f50
ongspxm <ongspxm@gmail.com>
2018-04-16 03:47:04 -0400

    wip. moving to module fb

--- fb/user.js
+++ fb/user.js
@@ -1,11 +1,4 @@
-/** index.js */
-
-const express = require("express");
-const request = require("request");
-const qstring = require("querystring");
-
-var app = express();
-
+/** fb/user.js */
 var fb_url = endpt => "https://graph.facebook.com/v2.12"+endpt;
 var fb_auth = "https://www.facebook.com/v2.12/dialog/oauth?";
 var fb_auth2 = "https://graph.facebook.com/v2.12/oauth/access_token?";
@@ -38,24 +31,3 @@ function getUserInfo(token){
         });
     });
 }
-
-app.get("/fb", function(req, res){
-    var query = req.query;
-    var opt = {
-        client_id: process.env.FB_APP_ID,
-        client_secret: process.env.FB_APP_SC,
-        redirect_uri: "https://"+req.headers.host+"/fb"
-    };
-
-    if (!query.code){ 
-        return getAuthURL(opt).then((url) => res.redirect(307, url));
-    }
-    
-    getAccessToken(query.code, opt)
-    .then(tkn => getUserInfo(tkn))
-    .then(info => res.send(info));
-});
-
-var listener = app.listen(3000, function(){
-    console.log("running on port "+listener.address().port);
-});
