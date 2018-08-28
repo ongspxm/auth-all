52a7935d857f650bd7365d08393dc8db563ad714
Metta Ong <ongspxm@gmail.com>
2018-08-28 22:58:49 +0800

    eslint changes for the main files

--- server.js
+++ server.js
@@ -1,94 +1,91 @@
-/** server.js **/
-const express = require("express");
-const request = require("request");
-const qstring = require("querystring");
+/** server.js * */
+const express = require('express');
 
-const jwt = require("jwt-simple");
-const admin = require("./libs/server_admin.js");
-const service = require("./libs/service.js");
+const admin = require('./libs/server_admin.js');
+const service = require('./libs/service.js');
 
-var app = express();
-app.use("/app", express.static("app/build"));
-app.use("/login", express.static("login/build"));
+const app = express();
+app.use('/app', express.static('app/build'));
+app.use('/login', express.static('login/build'));
 
-function errFn(res, err){
-    if(!process.env.DEBUG || !err){
-        err = "woah, watch out there";
-    }
+function errFn(res, err) {
+  let msg = err;
+  if (!process.env.DEBUG || !err) {
+    msg = 'woah, watch out there';
+  }
 
-    res.status(400);
-    res.send(err);
+  res.status(400);
+  res.send(msg);
 }
 
-/** fb authentication **/
-app.get("/fb", (req, res) => {
-    code = req.query["code"];
-    state = req.query["state"];
+/** fb authentication * */
+app.get('/fb', (req, res) => {
+  const { code, state } = req.query;
+  const { clientId: siteId } = req.query;
+  const { callback: callbackURL } = req.query;
 
-    siteId = req.query["clientId"];
-    callbackURL = req.query["callback"];
-
-    if(!code || !state){
-        if(!siteId || !callbackURL){
-            res.status(400);
-            return res.send("woah, watch out right there.");
-        }
-
-        service.fbIntiate(siteId, callbackURL)
-        .then(url => res.redirect(url))
-        .catch(err => errFn(err, res));
-    }else{
-        service.fbComplete(code, state)
-        .then(url => res.redirect(url))
-        .catch(err => errFn(res, err));
+  if (!code || !state) {
+    if (!siteId || !callbackURL) {
+      res.status(400);
+      return res.send('woah, watch out right there.');
     }
+
+    return service.fbIntiate(siteId, callbackURL)
+      .then(url => res.redirect(url))
+      .catch(err => errFn(err, res));
+  }
+  return service.fbComplete(code, state)
+    .then(url => res.redirect(url))
+    .catch(err => errFn(res, err));
 });
 
-/** admin endpoints **/
-function extractJwt(req){
-    return req.get("Authorization").split("Bearer ")[1];
+app.post('/post', (req, res) => {
+
+});
+
+/** admin endpoints * */
+function extractJwt(req) {
+  return req.get('Authorization').split('Bearer ')[1];
 }
 
-app.get("/signin", (req, res) => {
-    admin.signin("https://"+process.env.HOST+"/app")
+app.get('/signin', (req, res) => {
+  admin.signin(`https://${process.env.HOST}/app`)
     .then(url => res.redirect(url));
 });
 
-app.get("/getSites", (req, res) => {
-    admin.getSites(extractJwt(req))
+app.get('/getSites', (req, res) => {
+  admin.getSites(extractJwt(req))
     .then(sites => res.send(JSON.stringify(sites)))
     .catch(err => errFn(res, err));
 });
 
-app.get("/addSite", (req, res) => {
-    var domain = req.query.domain;
-    if(!domain){ return errFn(res); }
+app.get('/addSite', (req, res) => {
+  const { domain } = req.query;
+  if (!domain) { return errFn(res); }
 
-    admin.addSite(extractJwt(req), domain)
+  return admin.addSite(extractJwt(req), domain)
     .then(site => res.send(JSON.stringify(site)))
     .catch(err => errFn(res, err));
 });
 
-app.get("/delSite", (req, res) => {
-    var siteid = req.query.site;
-    var secret = req.query.secret;
-    if(!siteid || !secret){ return errFn(res); }
+app.get('/delSite', (req, res) => {
+  const { site: siteid, secret } = req.query;
+  if (!siteid || !secret) { return errFn(res); }
 
-    admin.delSite(extractJwt(req), siteid, secret)
+  return admin.delSite(extractJwt(req), siteid, secret)
     .then(() => res.send(JSON.stringify({})))
     .catch(err => errFn(res, err));
 });
 
-app.get("/genSecret", (req, res) => {
-    var siteid = req.query.site;
-    var secret = req.query.secret;
-    if(!siteid || !secret){ return errFn(res); }
+app.get('/genSecret', (req, res) => {
+  const { site: siteid, secret } = req.query;
+  if (!siteid || !secret) { return errFn(res); }
 
-    admin.genSecret(extractJwt(req), siteid, secret)
+  return admin.genSecret(extractJwt(req), siteid, secret)
     .then(() => res.send(JSON.stringify({})))
     .catch(err => errFn(res, err));
 });
 
-var listener = app.listen(3000, function(){
-    console.log("running on port "+listener.address().port);
+const listener = app.listen(3000, () => {
+  console.log(`running on port ${listener.address().port}`);
 });
--- setup.js
+++ setup.js
@@ -1,16 +1,12 @@
 /* setup.js */
-const dbase = require("./libs/dbase.js");
+const dbase = require('./libs/dbase.js');
 
 module.exports = dbase.setup()
-.then(() => dbase.delete("sites", "acct_id=?", [-1]))
-.then(() => {
-    return dbase.insert("sites", {
-        domain: process.env.HOST,
-        id: process.env.APP_ID,
-        secret: process.env.APP_SC,
-        acct_id: -1
-    });
-})
-.then(() => console.log("database done"));
-
-
+  .then(() => dbase.delete('sites', 'acct_id=?', [-1]))
+  .then(() => dbase.insert('sites', {
+    domain: process.env.HOST,
+    id: process.env.APP_ID,
+    secret: process.env.APP_SC,
+    acct_id: -1,
+  }))
+  .then(() => console.log('database done'));
